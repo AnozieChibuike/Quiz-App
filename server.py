dd68db80fc1f90ac75ffc6f9960c03bafb24b589
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-06-27 23:14:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 @app.route('/',methods=["POST","GET"])
 def index():
     if request.method == "POST":
         name = request.form['name']
-        return name
+        session['name'] = name
+        return redirect('/quiz')
     return render_template('index.html')
+
+@app.route('/quiz',methods=["POST","GET"])
+def quiz():
+    name = session.pop('name',None)
+    if request.method == "POST":
+        pass
+    return name
+    return render_template('quiz.html',name=name)
