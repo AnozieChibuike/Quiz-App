54465db139de8c1748c0eaf4377bffc08ccfb30b
AnozieChibuike <chibuikeanozie0@gmail.com>
2023-06-27 22:05:29 +0000

    Push

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/',methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        name = request.form['name']
+        return name
     return render_template('index.html')
